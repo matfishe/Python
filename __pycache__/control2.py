78908a646bd65653bf3dbd9054f91be117dab353
matfishe <msjfisher@outlook.com>
2022-04-27 19:00:56 -0400

    first commit

---
+++ __pycache__/control2.py
@@ -0,0 +1,16 @@
+x = 10
+y = 15
+
+while x < y:
+    print("Value of x is :",x)
+    answer = input("press y then enter to end.")
+
+    if answer == "y":
+        break
+    x += 1
+
+else:
+    print("the whole loop ran.")
+
+print("This is the end of my code")
+
---
+++ __pycache__/stringoperators.py
@@ -0,0 +1,30 @@
+#concatenate
+
+name1 = "Matt"
+
+name2 = "fisher"
+
+fullname = name1 + " " + name2
+
+print(fullname)
+
+#Repeat
+textvalue = "R"
+
+textvalue = textvalue * 5
+
+print(textvalue)
+
+
+#slice
+textvaluetwo = "Monday"
+textvaluetwo = textvaluetwo[3]
+
+print(textvaluetwo)
+
+
+#Range
+textvaluethree = "Monday"
+textvaluethree = textvaluethree[3:5]
+
+print(textvaluetwo)
